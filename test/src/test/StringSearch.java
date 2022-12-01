a2f3710df123ccc8a4cd1cf23189fbedb21fb651
Patrik <patriktao@gmail.com>
2022-12-01 05:01:42 +0100

    New Code

---
+++ test/src/test/StringSearch.java
@@ -0,0 +1,31 @@
+package test;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class StringSearch {
+
+	public static void main(String[] args) {
+		try {
+			String command = args[0];
+			String pattern = args[1];
+			String fileName = args[2];
+			BufferedReader reader = new BufferedReader(new FileReader(fileName));
+			 
+			for (String line = reader.readLine(); line != null; line = reader.readLine()) {
+				if(line.contains(pattern)) {
+					System.out.println(line);
+				}
+			}
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+}
